833b746abb74ca01d407836dd5496ba59be03b94
Devin <132347481+Devin0410@users.noreply.github.com>
2025-11-22 19:55:29 +0100

    Final

--- main.c
+++ main.c
@@ -3,8 +3,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World!\nRiFFfer\n");
-    return 0;
 
-    //dsadsasd
+    // Variablen deklarieren & initialiseren
+    int myNum = 4;
+    char myChar = 'D';
+    float myFloat = 4.21;
+    double myDouble = 3.141592653589793;
+
+  printf("Meine Lieblingszahl ist %d\n", myNum);
+  printf("Meine Lieblingszeichen ist %c\n", myChar);
+  printf("Meine Lieblingsfloat ist %.2f\n", myFloat);
+  printf("Meine Lieblingsdouble ist %.5lf\n", myDouble);
+  return 0;
 }
